c286ffd8dd5527671aba6d4db71a0e63edcd7cb1
Spids <spids@pop-os.localdomain>
2022-02-06 03:06:32 -0300

    mongoose deploy fix

---
+++ db/db.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose")
+const { MongoMemoryServer } = require("mongodb-memory-server")
+
+mongoose.Promise = Promise
+
+const mongoServer = new MongoMemoryServer()
+
+module.exports.getUri = async () => {
+    if (process.env.NODE_ENV === "test") {
+      return await mongoServer.getUri()
+    }
+    return `mongodb://productListUser:productListPassword@127.0.0.1:27017/promotions?authSource=admin`
+}
+
+module.exports.connect = async ({ uri }) => {
+    const mongooseOpts = {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    }
+
+    await mongoose.connect(uri, mongooseOpts)
+    
+    mongoose.connection.once("open", () => {
+        console.log(`MongoDB successfully connected to ${uri}`)
+    })
+}
+module.exports.closeDb = async () => {
+    await mongoose.disconnect()
+    
+    if (process.env.NODE_ENV === "test") {
+      await mongoServer.stop()
+    }
+}
--- src/producto/producto.test.js
+++ src/producto/producto.test.js
@@ -1,7 +1,35 @@
-const res = require("express/lib/response");
 const request = require("supertest")
-const { app,db } = require("../../server")
+const { app } = require("../../server")
+const Product = require('./producto')
+const { connect, getUri, closeDb } = require("../../db/db")
 
+beforeAll(async () => {
+    const uri = await getUri()
+        await connect({ uri })
+        let seed1= new Product({id:1,brand:'asd',description:'asdasd',image:'asdasdas',price:12312})
+        let seed2= new Product({
+            id: 181,
+            brand: "rvblsml",
+            description: "goeyxg nbowu",
+            image: "www.lider.cl/catalogo/images/toysIcon.svg",
+            price: 775722
+        })
+        let seed3= new Product({
+            id: 16,
+            brand: "fqqejoy",
+            description: "thyh mpzxgwnw",
+            image: "www.lider.cl/catalogo/images/gamesIcon.svg",
+            price: 525834
+        })
+        
+        await seed1.save()
+        await seed2.save()
+        await seed3.save()
+})
+  
+afterAll(async () => {
+    await closeDb()
+})
 describe('productos',() => {
     it('GET /productos retorna todos los productos', async () => {
         await request(app)
